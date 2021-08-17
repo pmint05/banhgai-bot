970795147f991fa890ea17c4797ac0a21f1ba9fe
Pham Thong <phucon547@gmail.com>
2021-08-17 22:13:13 +0700

    update

--- src/services/chatbotServices.js
+++ src/services/chatbotServices.js
@@ -31,29 +31,29 @@ let callSendAPI = (sender_psid, response) => {
 	);
 };
 
-let getUserName = async (sender_psid) => {
-	let username = "";
-	// Send the HTTP request to the Messenger Platform
-	await request(
-		{
-			uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
-			qs: { access_token: PAGE_ACCESS_TOKEN },
-			method: "GET",
-		},
-		(err, res, body) => {
-			console.log(body);
-			if (!err) {
-				response = JSON.parse(res);
-
-				// "first_name": "Peter",
-				// "last_name": "Chang",
-				username = `${response.first_name} ${response.last_name}`;
-			} else {
-				console.error("Unable to send message:" + err);
+let getUserName = (sender_psid) => {
+	return new Promise((resolve, reject) => {
+		// Send the HTTP request to the Messenger Platform
+		request(
+			{
+				uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+				method: "GET",
+			},
+			(err, res, body) => {
+				console.log(body);
+				if (!err) {
+					body = JSON.parse(body);
+					// "first_name": "Peter",
+					// "last_name": "Chang",
+					let username = `${body.first_name} ${body.last_name}`;
+					resolve(username);
+				} else {
+					console.error("Unable to send message:" + err);
+					reject(err);
+				}
 			}
-		}
-	);
-	return username;
+		);
+	});
 };
 
 let handleGetStarted = (sender_psid) => {
