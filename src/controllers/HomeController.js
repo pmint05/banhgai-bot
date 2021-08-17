d218934934dee3ec17c73f6e4db624a7199cb87d
Pham Thong <phucon547@gmail.com>
2021-08-17 16:29:02 +0700

    deploy heroku

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -118,13 +118,20 @@ function handlePostback(sender_psid, received_postback) {
 	let payload = received_postback.payload;
 
 	// Set the response based on the postback payload
-	if (payload === "yes") {
-		response = { text: "Thanks!" };
-	} else if (payload === "no") {
-		response = { text: "Oops, try sending another image." };
-	} else if (payload === "GET_STARTED") {
-		response = { text: "Hello" };
+	switch (payload) {
+		case "yes":
+			response = { text: "Thanks!" };
+			break;
+		case "no":
+			response = { text: "Oops, try sending another image." };
+			break;
+		case "GET_STARTED":
+			response = { text: "Chào bạn đã đến với page của mìnk =3" };
+			break;
+		default:
+			response = { text: `Oops, Xin lỗi tôi không hiểu ${payload}` };
 	}
+
 	// Send the message to acknowledge the postback
 	callSendAPI(sender_psid, response);
 }
