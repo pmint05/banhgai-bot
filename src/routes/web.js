412f0e47406a2955fe5f3baf19124c3ba588547a
Pham Thong <phucon547@gmail.com>
2021-08-17 16:07:28 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,11 +4,13 @@ import homeController from "../controllers/HomeController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", homeController.getHomePage);
+	router.get("/", homeController.getHomePage);
 
-    router.post('/webhook', homeController.postWebhook);
-    router.get('/webhook', homeController.getWebhook)
-    return app.use('/', router);
-}
+	router.post("/setup-profile", homeController.setupProfile);
+
+	router.post("/webhook", homeController.postWebhook);
+	router.get("/webhook", homeController.getWebhook);
+	return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
