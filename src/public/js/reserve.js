92f5165d41fe9944e48d7022da123ab8a2ab1ef9
Pham Thong <phucon547@gmail.com>
2021-08-22 21:33:21 +0700

    update

--- src/public/js/reserve.js
+++ src/public/js/reserve.js
@@ -41,17 +41,15 @@ window.extAsyncInit = function () {
 // }
 
 function handleClickReserveButton() {
-	$("#btnReserveTable").on("click", function (e) {
+	$("#btnReserve").on("click", function (e) {
 		// let check = validateInputFields();
 		let data = {
 			psid: $("#psid").val(),
 			fullName: $("#full-name").val(),
 			phoneNumber: $("#phone-number").val(),
-			address: `${$("#house-number").val()}, ${$("#ward").val()}, ${$(
-				"#district"
-			).val()}, ${$("#city").val()}`,
+			address: $("#address").val(),
 			note: $("#note").val(),
-			typeOfCake: $("#type").val(),
+			typeOfCake: $("#type").text(),
 			number: $("#number").val(),
 		};
 
@@ -83,57 +81,57 @@ function handleClickReserveButton() {
 	});
 }
 
-var districts = document.getElementById("district");
-var wards = document.getElementById("ward");
-var Parameter = {
-	url: "vn.json", //Đường dẫn đến file chứa dữ liệu hoặc api do backend cung cấp
-	method: "GET", //do backend cung cấp
-	responseType: "application/json", //kiểu Dữ liệu trả về do backend cung cấp
-};
-//gọi ajax = axios => nó trả về cho chúng ta là một promise
-var promise = axios(Parameter);
-//Xử lý khi request thành công
-promise.then(function (result) {
-	renderCity(result.data);
-});
+// var Parameter = {
+// 	url: "vn.json", //Đường dẫn đến file chứa dữ liệu hoặc api do backend cung cấp
+// 	method: "GET", //do backend cung cấp
+// 	responseType: "application/json", //kiểu Dữ liệu trả về do backend cung cấp
+// };
+// //gọi ajax = axios => nó trả về cho chúng ta là một promise
+// var promise = axios(Parameter);
+// //Xử lý khi request thành công
+// promise.then(function (result) {
+// 	renderCity(result.data);
+// });
 
-function renderCity(data) {
-	let citis = document.getElementById("city");
-	for (const x of data) {
-		citis.options[citis.options.length] = new Option(x.Name, x.Id);
-	}
+// function renderCity(data) {
+// 	let districts = document.getElementById("district");
+// 	let wards = document.getElementById("ward");
+// 	let citis = document.getElementById("city");
+// 	for (const x of data) {
+// 		citis.options[citis.options.length] = new Option(x.Name, x.Id);
+// 	}
 
-	// xứ lý khi thay đổi tỉnh thành thì sẽ hiển thị ra quận huyện thuộc tỉnh thành đó
-	citis.onchange = function () {
-		district.length = 1;
-		ward.length = 1;
-		if (this.value != "") {
-			const result = data.filter((n) => n.Id === this.value);
+// 	// xứ lý khi thay đổi tỉnh thành thì sẽ hiển thị ra quận huyện thuộc tỉnh thành đó
+// 	citis.onchange = function () {
+// 		district.length = 1;
+// 		ward.length = 1;
+// 		if (this.value != "") {
+// 			const result = data.filter((n) => n.Id === this.value);
 
-			for (const k of result[0].Districts) {
-				district.options[district.options.length] = new Option(
-					k.Name,
-					k.Id
-				);
-			}
-		}
-	};
+// 			for (const k of result[0].Districts) {
+// 				district.options[district.options.length] = new Option(
+// 					k.Name,
+// 					k.Id
+// 				);
+// 			}
+// 		}
+// 	};
 
-	// xứ lý khi thay đổi quận huyện thì sẽ hiển thị ra phường xã thuộc quận huyện đó
-	district.onchange = function () {
-		ward.length = 1;
-		const dataCity = data.filter((n) => n.Id === citis.value);
-		if (this.value != "") {
-			const dataWards = dataCity[0].Districts.filter(
-				(n) => n.Id === this.value
-			)[0].Wards;
+// 	// xứ lý khi thay đổi quận huyện thì sẽ hiển thị ra phường xã thuộc quận huyện đó
+// 	district.onchange = function () {
+// 		ward.length = 1;
+// 		const dataCity = data.filter((n) => n.Id === citis.value);
+// 		if (this.value != "") {
+// 			const dataWards = dataCity[0].Districts.filter(
+// 				(n) => n.Id === this.value
+// 			)[0].Wards;
 
-			for (const w of dataWards) {
-				wards.options[wards.options.length] = new Option(w.Name, w.Id);
-			}
-		}
-	};
-}
+// 			for (const w of dataWards) {
+// 				wards.options[wards.options.length] = new Option(w.Name, w.Id);
+// 			}
+// 		}
+// 	};
+// }
 jQuery(function ($) {
 	// MAD-RIPPLE // (jQ+CSS)
 	$(document).on("mousedown", "[data-ripple]", function (e) {
