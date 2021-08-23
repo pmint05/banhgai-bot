2ebdfc82065b6db7671d6b2e3467ffbe534334f0
Pham Thong <phucon547@gmail.com>
2021-08-23 17:19:14 +0700

    update

--- src/services/telegramServices.js
+++ src/services/telegramServices.js
@@ -5,11 +5,17 @@ let sendNotification = (recieved_data) => {
 		try {
 			let TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 			let TELEGRAM_GROUP_ID = process.env.TELEGRAM_GROUP_ID;
-
+			if (recieved_data.note === "") {
+				recieved_data.note = "<Không có>";
+			}
+			recieved_data.phoneNumber = recieved_data.phoneNumber.replace(
+				"'",
+				""
+			);
 			let data = {
 				chat_id: TELEGRAM_GROUP_ID,
 				parse_mode: "HTML",
-				text: `| --- Một đơn đặt mới --- |\n| -------------------- |\n| 0.Tên Facebook: <b>${recieved_data.username}</b> |\n| 1.Họ & tên: <b>${recieved_data.fullName}</b> |\n| 2.Số điện thoại: <b>${recieved_data.phoneNumber}</b> |\n| 3.Địa chỉ: <b>${recieved_data.address}</b> |\n| 4.Loại bánh: <b>${recieved_data.typeOfCake}</b> |\n| 5.Số lượng: <b>${recieved_data.number}</b> |\n| 6.Ghi chú: <b>${recieved_data.note}</b> |\n`,
+				text: `| --- Một đơn đặt mới --- |\n| ---------------------------------------- |\n| 0.Tên Facebook: <b>${recieved_data.username}</b> |\n| 1.Họ & tên: <b>${recieved_data.fullName}</b> |\n| 2.Số điện thoại: <b>${recieved_data.phoneNumber}</b> |\n| 3.Địa chỉ: <b>${recieved_data.address}</b> |\n| 4.Loại bánh: <b>${recieved_data.typeOfCake}</b> |\n| 5.Số lượng: <b>${recieved_data.number}</b> |\n| 6.Ghi chú: <b>${recieved_data.note}</b> |\n`,
 			};
 			// https://api.telegram.org/bot1954437611:AAHpub5JP2ksiT-S-NHZSodOfWq-4nll3Us/sendMessage?chat_id=-1001514909519&text=welcome
 			request(
